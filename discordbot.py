542815f0772788c2450d447b43fb906dbdf2c0ff
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-06-17 21:48:06 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,29 @@
 from cmath import log
 from distutils.sysconfig import PREFIX
 import discord
+from discord.ext import commands
+from discord_slash import SlashCommand
+
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-PREFIX = os.environ['PREFIX']
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+slash = SlashCommand(bot, sync_commands=True)
+
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print("Bot is ready!")
+    print(f"Synced {len(bot.slash.commands)} command(s)")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@slash.slash(name="hello", description="Say hello")
+async def hello(ctx):
+    await ctx.send(f"Hey {ctx.author.mention}! This is a slash command!")
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+@slash.slash(name="say", description="Echo a message")
+async def say(ctx, thing_to_say: str):
+    await ctx.send(f"{ctx.author.name} said: `{thing_to_say}`")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
-
-
-try:
-    client.run(TOKEN)
-except discord.errors.LoginFailure as e:
-    print("Improper token has been passed.")
+bot.run(TOKEN)
