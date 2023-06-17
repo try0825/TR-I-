54f5355d42a231ba59e97a757a47d00313638c81
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-06-17 20:46:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,28 +1,32 @@
-
+from cmath import log
+from distutils.sysconfig import PREFIX
 import discord
-from discord import app_commands
-from discord.ext import commands
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
-@bot.event
+
+client = discord.Client()
+
+@client.event
 async def on_ready():
-	print("Bot is ready!")
-	try:
-		synced = await bot.tree.sync()
-		print(f"Synced {len(synced)} commands(s)")
+    print(f'Logged in as {client.user}.')
 
-	except Exception as e:
-		print(e)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-@bot.tree.command(name="hello")
-async def hello(interaction: discord.Interaction):
-	await interaction.response.send_message(f"Hey {interaction.user.mention}! This is a slash command!", ephemeral=True)
+    if message.content == f'{PREFIX}call':
+        await message.channel.send("callback!")
 
-@bot.tree.command(name="say")
-@app_commands.describe(thing_to_say = "what should I say?")
-async def say(interaction: discord.Interaction, thing_to_say: str):
-	await interaction.response.send_message(f"{interaction.user.name} said: `{thing_to_say}`")
+    if message.content.startswith(f'{PREFIX}hello'):
+        await message.channel.send('Hello!')
 
-bot.run(TOKEN)
 
+try:
+    client.run(TOKEN)
+except discord.errors.LoginFailure as e:
+    print("Improper token has been passed.")
