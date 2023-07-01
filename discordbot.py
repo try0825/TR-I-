39fd0c237192f82ed291e4d8e3d20a7828d67b24
hayul0629 <120244649+hayul0629@users.noreply.github.com>
2023-07-01 20:56:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,9 +31,9 @@ def main(in_gamever, in_transfer_code, in_confirmation_code, in_catfood):
     game_version = helper.str_to_gv(game_version_input)
     transfer_code = in_transfer_code
     confirmation_code = in_confirmation_code
-    save_data = server_handler.download_save(country_code, transfer_code, confirmation_code, game_version)
-
     try:
+        save_data = server_handler.download_save(country_code, transfer_code, confirmation_code, game_version)
+
         save_data = patcher.patch_save_data(save_data, country_code)
         save_stats = parse_save.start_parse(save_data, country_code)
 
@@ -45,7 +45,7 @@ def main(in_gamever, in_transfer_code, in_confirmation_code, in_catfood):
         print("===================================================================================")
         print(e)
         print("===================================================================================")
-
+        pass
 @bot.event
 async def on_ready():
 	print("Bot is ready!")
@@ -59,67 +59,98 @@ async def on_ready():
 @bot.tree.command(name="catfood", description="계정에 통조림 충전")
 @app_commands.describe(gamever = "게임 버전(eg. 12.4)", transfer_code = "이어하기코드 입력", confirmation_code = "인증번호 입력",catfood =  "원하는 통조림값(MAX:45000)")
 async def hello(interaction: discord.Interaction,gamever: str, transfer_code: str, confirmation_code: str, catfood: str):
-    m_channel = interaction.channel.id
-    author_id = interaction.user.id
-    p_user = interaction.user
-    point = points.get(p_user.id, 0)
-    if m_channel == 1122288430664130560:
-        if point >= 1:
-            points[p_user.id] -= 1
-            await interaction.response.send_message(f"통조림 {catfood}개 충전이 요청되었습니다.", ephemeral=False)
-            tran,pin = main(gamever, transfer_code, confirmation_code, catfood)
-            await interaction.user.send(f"이어하기코드: {tran}\n인증번호: {pin}\n<#1119451755713941585> 꼭 작성해주세요.")
+    try:
+        m_channel = interaction.channel.id
+        author_id = interaction.user.id
+        p_user = interaction.user
+        point = points.get(p_user.id, 0)
+        if m_channel == 1122288430664130560:
+            if point >= 1:
+                points[p_user.id] -= 1
+                await interaction.response.send_message(f"통조림 {catfood}개 충전이 요청되었습니다.", ephemeral=False)
+                tran,pin = main(gamever, transfer_code, confirmation_code, catfood)
+                await interaction.user.send(f"이어하기코드: {tran}\n인증번호: {pin}\n<#1119451755713941585> 꼭 작성해주세요.")
+            else:
+                await interaction.response.send_message(f"실링이 부족합니다. (현제 보유 실링: **{point}**)", ephemeral=True)
         else:
-            await interaction.response.send_message(f"실링이 부족합니다. (현제 보유 실링: **{point}**)", ephemeral=True)
-    else:
-        await interaction.response.send_message("통조림 충전 요청은 <#1122288430664130560>에서 해주세요.", ephemeral=True)
+            await interaction.response.send_message("통조림 충전 요청은 <#1122288430664130560>에서 해주세요.", ephemeral=True)
+    except Exception as e:
+	points[p_user.id] += 1
+        embedVar = discord.Embed(title="계정 오류", color=0xffec42)
+        embedVar.add_field(name="",value="이어하기코드,인증번호를 다시 확인해주세요.",inline=False)
+        embedVar.add_field(name="",value="사용된 실링은 복구됩니다.",inline=False)
+	e_channel = bot.get_channel(1122289081381031976)
+	e_channel.send(f"<@{interaction.user.id}>",embed=embedVar)
+        print("오류 발생")
+        print("===================================================================================")
+        print(e)
+        print("===================================================================================")
+	pass
 @bot.tree.command(name="my_siling", description="내 실링 확인하기")
 async def hello(interaction: discord.Interaction):
-    p_user = interaction.user
-    point = points.get(p_user.id, 0)
-    await interaction.response.send_message(f"{interaction.user.name}님의 보유 실링은 **{point}sl**입니다.", ephemeral=True)
+    try:
+        p_user = interaction.user
+        point = points.get(p_user.id, 0)
+        await interaction.response.send_message(f"{interaction.user.name}님의 보유 실링은 **{point}sl**입니다.", ephemeral=True)
+    except Exception as e:
+        print("오류 발생")
+        print("===================================================================================")
+        print(e)
+        print("===================================================================================")
 @bot.tree.command(name="free_siling", description="무료 실링 받기")
 async def hello(interaction: discord.Interaction):
-    m_channel = interaction.channel.id
-    author_id = interaction.user.id
-    if m_channel == 1119580806856310795:
-        if author_id in user_dict and time.time() - user_dict[author_id] < cooltime:
-            cool_time = round(user_dict[author_id] + cooltime - time.time())
-            wait_time = convert_time(cool_time)        
-            await interaction.response.send_message(f"무료실링 요청 재대기시간이 {wait_time} 남았습니다.", ephemeral=True)
-            return
+    try:
+        m_channel = interaction.channel.id
+        author_id = interaction.user.id
+        if m_channel == 1119580806856310795:
+            if author_id in user_dict and time.time() - user_dict[author_id] < cooltime:
+                cool_time = round(user_dict[author_id] + cooltime - time.time())
+                wait_time = convert_time(cool_time)        
+                await interaction.response.send_message(f"무료실링 요청 재대기시간이 {wait_time} 남았습니다.", ephemeral=True)
+                return
+            else:
+                user_dict[author_id] = time.time()
+                points[interaction.user.id] = points.get(interaction.user.id, 0) + 1
+                await interaction.response.send_message(f"무료실링이 요청되었습니다.", ephemeral=False)
         else:
-            user_dict[author_id] = time.time()
-            points[interaction.user.id] = points.get(interaction.user.id, 0) + 1
-            await interaction.response.send_message(f"무료실링이 요청되었습니다.", ephemeral=False)
-    else:
-        await interaction.response.send_message("무료실링 요청은 <#1119580806856310795>에서 해주세요.", ephemeral=True)
+            await interaction.response.send_message("무료실링 요청은 <#1119580806856310795>에서 해주세요.", ephemeral=True)
+    except Exception as e:
+        print("오류 발생")
+        print("===================================================================================")
+        print(e)
+        print("===================================================================================")
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content.startswith('&s') and message.author.id in admin_ids:
-        p_user = message.author
-        point = points.get(p_user.id, 0)
-        points[p_user.id] =+ 100
-        await message.delete()
-    if message.content.startswith('!a') and message.author.id in admin_ids:
-        p_user = message.author
-        point = points.get(p_user.id, 0)
-        member = message.mentions[0]
-        points[member.id] = points.get(member.id, 0) + 1
-        embedVar = discord.Embed(title="실링 추가", color=0x00ff26)
-        embedVar.add_field(name="",value=f"{member.name}님에게 **1sl**를 추가하였습니다.\n**잔여 :{points[member.id]}sl**",inline=False)
-        await message.channel.send(embed=embedVar)
-        await message.delete()
-    if message.content.startswith('!d') and message.author.id in admin_ids:
-        p_user = message.author
-        point = points.get(p_user.id, 0)
-        member = message.mentions[0]
-        points[member.id] = points.get(member.id, 0) - 1
-        embedVar = discord.Embed(title="실링 추가", color=0x00ff26)
-        embedVar.add_field(name="",value=f"{member.name}님에게 **1sl**를 차감하였습니다.\n**잔여 :{points[member.id]}sl**",inline=False)
-        await message.channel.send(embed=embedVar)
-        await message.delete()
+    try:
+        if message.author == bot.user:
+            return
+        if message.content.startswith('&s') and message.author.id in admin_ids:
+            p_user = message.author
+            point = points.get(p_user.id, 0)
+            points[p_user.id] =+ 100
+            await message.delete()
+        if message.content.startswith('!a') and message.author.id in admin_ids:
+            p_user = message.author
+            point = points.get(p_user.id, 0)
+            member = message.mentions[0]
+            points[member.id] = points.get(member.id, 0) + 1
+            embedVar = discord.Embed(title="실링 추가", color=0x00ff26)
+            embedVar.add_field(name="",value=f"{member.name}님에게 **1sl**를 추가하였습니다.\n**잔여 :{points[member.id]}sl**",inline=False)
+            await message.channel.send(embed=embedVar)
+            await message.delete()
+        if message.content.startswith('!d') and message.author.id in admin_ids:
+            p_user = message.author
+            point = points.get(p_user.id, 0)
+            member = message.mentions[0]
+            points[member.id] = points.get(member.id, 0) - 1
+            embedVar = discord.Embed(title="실링 추가", color=0x00ff26)
+            embedVar.add_field(name="",value=f"{member.name}님에게 **1sl**를 차감하였습니다.\n**잔여 :{points[member.id]}sl**",inline=False)
+            await message.channel.send(embed=embedVar)
+            await message.delete()
+    except Exception as e:
+        print("오류 발생")
+        print("===================================================================================")
+        print(e)
+        print("===================================================================================")
 if __name__ == "__main__":
     bot.run(TOKEN)
